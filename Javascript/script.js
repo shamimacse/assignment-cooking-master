b85906cb09576e3ac9bcecb5524d0a33ce049300
shamimacse <shamima.cse01@gmail.com>
2021-02-07 20:53:31 +0600

    final comment

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,28 +1,30 @@
 document.querySelector('#search-btn').addEventListener("click",function(){
     const foodMenu = document.getElementById('search-field').value;
+    
     if(foodMenu!=''){
-        fetchMealAPI(foodMenu);
+        fetchMealItem(foodMenu);
     }else{
         console.log("Empty");
     }
-})
-
-function fetchMealAPI(foodMenu){
+      })
+function fetchMealItem(foodMenu){
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodMenu}`)
     .then(res => res.json())
-    .then(data => showMeal(data.meals))
+    .then(data => displayMeal(data.meals))
 }
 
-function showMeal(data){
-    const mealsSection = document.getElementById('food-details');
+function displayMeal(data){
+    const displayFoodArea = document.getElementById('food-details');
+
     data.forEach(mealsInfo => {
         const div = document.createElement('div');
-        div.className = 'img-title' ; 
-        const mealInfo = `<img src=${mealsInfo.strMealThumb}>
-            <p class="title">${mealsInfo.strMeal}</p>`;
+        div.className = 'menu-item' ; 
 
-            div.innerHTML = mealInfo;
-            mealsSection.appendChild(div);
+        const FoodMenuItem = `<img src=${mealsInfo.strMealThumb}>
+            <p class="food-item">${mealsInfo.strMeal}</p>`;
+
+            div.innerHTML = FoodMenuItem;
+            displayFoodArea.appendChild(div);
         });
     }
     
